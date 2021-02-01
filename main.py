5e36d61ffc3419bc634db62d09a59a61a63249c8
Obi Chukwuemeka <oc47@humboldt.edu>
2021-02-01 10:00:49 -0800

    scrape info from rottentomate, add bs4, csv

---
+++ main.py
@@ -0,0 +1,29 @@
+from urllib.request import urlopen   
+from bs4 import BeautifulSoup
+
+url_to_scrape =  "https://editorial.rottentomatoes.com/guide/best-netflix-movies-to-watch-right-now/" 
+
+request_page = urlopen(url_to_scrape) #requesting the page
+page_html = request_page.read()       #read the request
+request_page.close()     #closing the request for us
+soup_data = BeautifulSoup(page_html, 'html.parser')
+soup_data.title.text
+movies = soup_data.find_all('div',{'class':'col-sm-18 col-full-xs countdown-item-content'} )
+first_movie = movies[0]
+first_movie.find('div', {"class":"countdown-index"}).text #rank
+first_movie.h2.a.text #Name of the movie
+first_movie.find('span', {"class":"subtle start-year"}).text[1:5] #year
+first_movie.find('span', {"class":"tMeterScore"}).text #tmeter
+
+filename = 'help.csv' #passing it to a csv
+f = open(filename, 'w') #open filename with rate permission
+headers = 'Rank, Name, Year, tmeta \n'
+f.write(headers)
+for shows in movies:
+    Rank = shows.find('div', {"class":"countdown-index"}).text
+    Name = shows.h2.a.text
+    Year = shows.find('span', {"class":"subtle start-year"}).text[1:5]
+    tmeta = shows.find('span', {"class":"tMeterScore"}).text 
+    
+    f.write(Rank + ',' + Name + ',' + Year+ ',' + tmeta)
+f.close()
